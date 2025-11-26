f1e150710534f5359e023d5a4cc7e0a560046ea8
Hari Krishna <mangalampalliharikrishna8@gmail.com>
2025-11-26 14:39:55 +0530

    Refactor - Pass the tests for multiple digits with comma delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,8 +3,14 @@ class StringCalculator
   # {params} - String
   # {return} - Integer
   def self.add(string)
-    return 0 if string.empty?
-    numbers = string.strip.split(',').map(&:to_i)
+    return 0 if string.strip.empty?
+    numbers = parse_numbers(string)
     numbers.sum
+  end
+
+  private
+
+  def self.parse_numbers(string)
+    string.strip.split(',').map(&:to_i)
   end
 end
