bb893ee160262739db7b7ea3d5cf6b61fb4613fe
Hari Krishna <mangalampalliharikrishna8@gmail.com>
2025-11-26 15:55:01 +0530

    Green - Pass the negative digits exception test

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -16,13 +16,16 @@ class StringCalculator
     if string.start_with? "//"
       custom_delimiter_string, string_rest = string.split("\n", 2)
       raise ArgumentError, "Invalid input: missing digits string after delimiter declaration" if string_rest.nil?
-      
+
       custom_delimiter = custom_delimiter_string.chars.last
       delimiters << custom_delimiter
       string = string_rest
     end
 
     pattern = Regexp.union(delimiters)
-    string.strip.split(pattern).map(&:to_i)
+    string.strip.split(pattern).map do |number|
+      raise ArgumentError, "negative numbers not allowed (#{number})" if number.to_i < 0
+      number.to_i
+    end
   end
 end
